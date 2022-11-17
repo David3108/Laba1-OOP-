4fff339721773c6202b66f25e6fb75abf447eabd
David <shepetkodavid@gmail.com>
2022-11-17 10:22:50 +0200

    first commit

---
+++ GameAccount.cs
@@ -0,0 +1,101 @@
+﻿namespace HelloWorld
+{
+    class GameAccount
+    {
+        private string UserName { get; set; }
+        public string GetUserName {
+            get
+            {
+                return UserName;
+            }
+        }
+        private int CurrentRating { get; set; }           
+        private int GamesCount { get; set; }
+        //Створення списку, у якому буде історія про користувача
+
+        private List<History> history = new List<History>() ;
+        public static String WIN = "Win";
+        public static String LOSE = "Lose";
+
+        public GameAccount(string name)
+        {
+            UserName = name;
+            CurrentRating = 20;
+            GamesCount = 0;
+        }
+        public void WinGame(String opponentName, int rating) {
+            if (rating < 0)
+            {
+                throw new Exception("Rating cannot be negative!");
+                return;
+            }
+
+            History UserHistory = new History();
+                UserHistory.OpponentName = opponentName;
+                UserHistory.Result = WIN;
+                UserHistory.Rating = rating;
+                UserHistory.GamesCount = ++GamesCount;
+                CurrentRating += rating;
+                history.Add(UserHistory);
+        }
+        public void LoseGame(String opponentName, int rating)
+        {
+            if (rating < 0)
+            {
+                throw new Exception("Rating cannot be negative!");
+                return;
+            }          
+                History UserHistory = new History();
+                UserHistory.OpponentName = opponentName;
+                UserHistory.Result = LOSE;
+                UserHistory.Rating = rating;
+                UserHistory.GamesCount = ++GamesCount;
+                if (CurrentRating - rating < 1)
+                {
+                    CurrentRating = 1;
+                }
+                else
+                {
+                    CurrentRating -= rating;
+                }
+                history.Add(UserHistory);           
+
+        }
+
+        public string GetStats()
+        {
+            var report = new System.Text.StringBuilder();
+
+            report.AppendLine("Game Index |Opponent Name |Rating |Result ");
+            foreach (var item in history)
+            {
+                report.AppendLine($" {item.GamesCount} - {item.OpponentName} - {item.Rating} - {item.Result}\t");
+            }
+            return report.ToString();
+        }
+
+        public int CurRating()
+        {
+            return CurrentRating;
+        }     
+        
+
+    }
+//Створення класу, у якому буде історія про користувача
+
+    class History
+    {
+        public String UserName { get; set; }
+        public String OpponentName { get; set; }
+        public string Result { get; set; }
+        public int Rating { get; set; }
+        private int gamesCount = 0;
+         public int GamesCount { get; set; }
+         /*{
+             get { return GamesCount; }
+             set { GamesCount = value; }
+         }*/
+         
+        
+    }
+}
---
+++ Start.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Start
+    {
+        
+        static void Main(string[] args)
+        {         
+            GameAccount user1 = new GameAccount("Vasya");
+            GameAccount user2 = new GameAccount("Vitalii");
+            int number;
+            int rating; 
+            Console.Write("Enter rating: ");
+            rating = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            number = rnd.Next(1,12);
+            for (int i = 0; i < 3; i++)
+            {
+                
+                if (number >= 6) 
+                {                
+                    try
+                    {
+                        user1.WinGame("Vitalii", rating);
+                        user2.LoseGame("Vasya", rating);
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex.Message);
+                        return;
+                    }
+                    number = rnd.Next(1,12);
+                }
+                else
+                {
+                    try
+                    {
+                        user1.LoseGame("Vitalii", rating);
+                        user2.WinGame("Vasya", rating);
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex.Message);
+                        return;
+                    }
+                    number = rnd.Next(1,12);
+                }
+            }
+            Console.Write($"{user1.GetUserName} history is:\n");
+            Console.Write(user1.GetStats());
+            Console.Write($"{user2.GetUserName} history is:\n");
+            Console.Write(user2.GetStats());
+            Console.WriteLine($"Vasya rating:{user1.CurRating()}");
+            Console.WriteLine($"Vitalii rating:{user2.CurRating()}");
+            
+        }
+    }
+}
